ebc27719ff535a8a6939145f4b27aa218d9e1530
Andrew Shellberg <56370289+Shellb1@users.noreply.github.com>
2024-04-28 21:03:49 -0500

    initial commit

---
+++ ncbca-reference-frontend/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { SeasonComponent } from './season/season.component';
+
+export const routes: Routes = [
+    {
+        path: 'season', component: SeasonComponent
+    }
+];
