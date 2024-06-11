ea1c1c22173b2ed13b4f6fa531aad176ea24f814
Andrew Shellberg <56370289+Shellb1@users.noreply.github.com>
2024-06-11 18:35:16 -0500

    refactoring, adding NT seed to seaon summary

--- ncbca-reference-frontend/src/app/model/Season.ts
+++ ncbca-reference-frontend/src/app/model/Season.ts
@@ -1,24 +1,26 @@
 import { SeasonMetrics } from "./SeasonMetrics";
 
 export class Season {
-    teamId: number;
-    teamName: string;
-    gamesWon: number;
-    gamesLost: number;
-    seasonYear: number;
-    coach: string;
-    conferenceName: string;
-    seasonMetrics: SeasonMetrics
-  
-    constructor(teamId: number, teamName: string, gamesWon: number, gamesLost: number, seasonYear: number, coach: string, conferenceName: string, seasonMetrics: SeasonMetrics) {
-      this.teamId = teamId;
-      this.teamName = teamName;
-      this.gamesWon = gamesWon;
-      this.gamesLost = gamesLost;
-      this.seasonYear = seasonYear;
-      this.coach = coach;
-      this.conferenceName = conferenceName;
-      this.seasonMetrics = seasonMetrics;
-    }
-  
+  teamId: number;
+  teamName: string;
+  gamesWon: number;
+  gamesLost: number;
+  seasonYear: number;
+  coach: string;
+  conferenceName: string;
+  seasonMetrics: SeasonMetrics;
+  ntSeed: number;
+
+  constructor(teamId: number, teamName: string, gamesWon: number, gamesLost: number, seasonYear: number, coach: string, conferenceName: string, seasonMetrics: SeasonMetrics, ntSeed: number) {
+    this.teamId = teamId;
+    this.teamName = teamName;
+    this.gamesWon = gamesWon;
+    this.gamesLost = gamesLost;
+    this.seasonYear = seasonYear;
+    this.coach = coach;
+    this.conferenceName = conferenceName;
+    this.seasonMetrics = seasonMetrics;
+    this.ntSeed = ntSeed;
   }
+
+}
